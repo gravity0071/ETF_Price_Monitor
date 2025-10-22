b5fe1c740517bd732f21759e6a09736bf42128d2
Shawn Wan <78680043+gravity0071@users.noreply.github.com>
2025-10-22 15:31:06 -0400

    added a session store to allow user use the change the chart timeLine without uploading the etf.csv again.

---
+++ etf_backend/app/services/session_store.py
@@ -0,0 +1,27 @@
+from uuid import uuid4
+import pandas as pd
+from typing import Dict, Optional
+
+class etf_session_store:
+    def __init__(self):
+        self.sessions: Dict[str, pd.DataFrame] = {}
+
+    def create_or_update_session(self, merged_df: pd.DataFrame, session_id: Optional[str] = None) -> str:
+        # print(session_id)
+        # print(self.sessions)
+        if session_id and session_id in self.sessions:
+            self.sessions[session_id] = merged_df
+            print("[INFO] Updated existing ETF session: {}", session_id)
+            return session_id
+        new_id = str(uuid4())
+        self.sessions[new_id] = merged_df
+        print("[INFO] Created new ETF session: {}",new_id)
+        return new_id
+
+    def get_session(self, session_id: str) -> Optional[pd.DataFrame]:
+        return self.sessions.get(session_id)
+
+    def clear(self, session_id: str):
+        if session_id in self.sessions:
+            del self.sessions[session_id]
+            print(f"[INFO] Cleared ETF session: {session_id}")
